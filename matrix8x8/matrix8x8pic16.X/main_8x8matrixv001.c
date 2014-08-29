54922cbe1ab4f20138499fa8d5c27753df54da29
Julio Alexander Aguilar Angulo <juloaguilar@gmail.com>
2014-08-29 14:51:18 +0200

    Ver 0.01c submitted
    BETATESTERS (HW and SW) needed!!
    * History:
    *
    *      V0.01a: Initial Dummy Code template and structured definitions
    *             - hardware target: Pedro Sanchez board (available as
    sch-brd)
    *      V0.01b: Spi-Like Shift Register Function Added inside main file
    *      V0.01c: Matrix Test initial Code.

--- matrix8x8/matrix8x8pic16.X/main_8x8matrixv001.c
+++ matrix8x8/matrix8x8pic16.X/main_8x8matrixv001.c
@@ -12,9 +12,11 @@
  *
  * History:
  *
- *      V0.01: Initial Dummy Code template and structured definitions
+ *      V0.01a: Initial Dummy Code template and structured definitions
  *             - hardware target: Pedro Sanchez board (available as sch-brd)
- * 
+ *      V0.01b: Spi-Like Shift Register Function Added inside main file
+ *      V0.01c: Matrix Test initial Code.
+ *
  * Created on 29 août 2014, 10:25
  * Licensing based on the MIT License.
  *  */
@@ -28,9 +30,12 @@
 #pragma config CP=OFF, CPD=OFF, BOREN=OFF, WDTE=OFF
 #pragma config PWRTE=OFF, FOSC=INTOSCIO, MCLRE=OFF, LVP=OFF
 
+#define _XTAL_FREQ 4000000 // in order to use the built-in delay (to change later)
+        
 char    video_buffer[64];
 char    temp_X,temp_Y;
-
+char    i;
+void SendData(char data);
 
 void main(void) {
 
@@ -43,8 +48,29 @@ void main(void) {
 
 
     while(1){
-
+        SendData(ColumnScan[1]);
+        RowPort=Patterns[i];
+        i++;i&=0b111;
+        __delay_ms(20);
 
     }
     return;
 }
+
+
+void SendData(char data){
+unsigned char LOOP, FLAG;
+
+RowPort=0x00;
+for (LOOP=0; LOOP<8; LOOP++)
+    {
+     FLAG=data&0x01;
+        if(FLAG==0) SpiData=0;
+        else    SpiData=1;
+
+    SpiClock = 1;
+    SpiClock = 0;
+    data >>= 1;
+    }
+}
+
